662652269011b04aaaba27ec6a77bd8ac1daaee8
muyuan.zhai <muyuan.zhai@datayes.com>
2017-11-16 16:32:47 +0800

    Myron: fix single linked list.

--- lists/single_linked_list.py
+++ lists/single_linked_list.py
@@ -1,8 +1,7 @@
 # -*- coding: UTF-8 -*-
 # **********************************************************************************#
-#     File: Single linked list file
+#     File: Data structure utils
 # **********************************************************************************#
-from copy import copy
 
 
 def recursive(formula, formatter=(lambda x: None)):
@@ -10,21 +9,33 @@ def recursive(formula, formatter=(lambda x: None)):
         def _recursive(node, *args, **kwargs):
             if node.tail:
                 return formula(_recursive(node.tail, *args, **kwargs), formatter(node))
+
             return func(node, *args, **kwargs)
+
         return _recursive
+
     return decorator
 
 
-def traversal(func):
-    def _traversal(node, *args, **kwargs):
-        while node:
-            func(node, *args, **kwargs)
-            node = node.tail
-    return _traversal
+def traversal(returnable=True):
+
+    def decorator(func):
+        def _traversal(node, *args, **kwargs):
+            while node:
+                func(node, *args, **kwargs)
+                node = node.tail
+
+        def _traversal_returnable(node, *args, **kwargs):
+            result = list()
+            while node:
+                result.append(func(node, *args, **kwargs))
+                node = node.tail
+            return result
+        return _traversal_returnable if returnable else _traversal
+    return decorator
 
 
 class Node(object):
-
     def __init__(self, obj, head=None, tail=None):
         self.head = head
         self.tail = tail
@@ -35,14 +46,14 @@ class Node(object):
         node.head = self.tail
 
 
-class SingleLinkedList(object):
+class LinkedList(object):
 
     def __init__(self, link_head=None, link_tail=None):
         self.link_head = link_head
         self.link_tail = link_tail
 
     def __add__(self, other):
-        assert isinstance(other, SingleLinkedList), 'Invalid item to add.'
+        assert isinstance(other, LinkedList), 'Invalid item to add.'
         self.link_tail = self.link_head if self.link_tail is None else self.link_tail
         self.link_tail.tail = other.link_head
         self.link_tail = other.link_tail
@@ -53,13 +64,27 @@ class SingleLinkedList(object):
 
     def append(self, node):
         if not self.link_head:
-            self.link_head = self.link_tail = copy(node)
+            self.link_head = self.link_tail = node
         else:
-            self.link_tail.tail = self.link_tail = copy(node)
+            node.head = self.link_tail
+            self.link_tail.tail = self.link_tail = node
 
     def extend(self, nodes):
         for node in nodes:
             self.append(node)
+
+    def delete(self, node):
+        if not node.head and not node.tail:
+            self.link_head = self.link_tail = None
+        elif not node.head and node.tail:
+            self.link_head = node.tail
+            node.tail.head = None
+        elif node.head and not node.tail:
+            node.head.tail = None
+            self.link_tail = node.head
+        else:
+            node.head.tail = node.tail
+            node.tail.head = node.head
 
     def recursive(self, formula, formatter):
 
@@ -71,11 +96,13 @@ class SingleLinkedList(object):
 
     def traversal(self, func, *args, **kwargs):
 
-        @traversal
+        @traversal(returnable=True)
         def executor(node, *inner_args, **inner_kwargs):
-            func(node, *inner_args, **inner_kwargs)
+            return func(node, *inner_args, **inner_kwargs)
 
         return executor(self.link_head, *args, **kwargs)
 
     def get_length(self):
+        if not self.link_head:
+            return 0
         return self.recursive(formula=(lambda x, y: x + y), formatter=(lambda x: 1))
